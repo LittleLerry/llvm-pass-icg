51b6ec59a74c9c10bd2884ae90dbd3f93a7a58d1
LittleLerry <76429680+LittleLerry@users.noreply.github.com>
2023-10-28 01:16:42 +0800

    Delete build directory

--- build/icg/CMakeFiles/install-icgPass-stripped.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-icgPass-stripped"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-icgPass-stripped.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
