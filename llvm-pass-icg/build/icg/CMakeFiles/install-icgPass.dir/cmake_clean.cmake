ae6fda5145279192e9b65e28a60ca2f33260bf1c
LittleLerry <76429680+LittleLerry@users.noreply.github.com>
2023-10-28 01:03:22 +0800

    Delete llvm-pass-icg directory

--- llvm-pass-icg/build/icg/CMakeFiles/install-icgPass.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-icgPass"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-icgPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
