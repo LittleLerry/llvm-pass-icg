50e4032992bbbc983d29f5c62709e1491ce226b5
LittleLerry <76429680+LittleLerry@users.noreply.github.com>
2023-10-28 01:04:46 +0800

    Add files via upload

---
+++ build/icg/CMakeFiles/install-icgPass-stripped.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/install-icgPass-stripped"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/install-icgPass-stripped.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/icg/CMakeFiles/install-icgPass.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/install-icgPass"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/install-icgPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
